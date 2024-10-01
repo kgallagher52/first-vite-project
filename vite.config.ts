52e944a225ad6a77a5700fa4cb31be223bc396d2
kgallagher52 <keatonrgallagher@gmail.com>
2024-09-30 18:22:39 -0600

    feat(creating-multiple): routes

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				nested: resolve(__dirname, "index.html"),
+				nested: resolve(__dirname, "another-route/index.html"),
 			},
 		},
 	},
